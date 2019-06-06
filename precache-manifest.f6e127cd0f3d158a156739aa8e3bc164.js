a9a3ed46fda0b873fd46726f79264ea52409a9a3
Kacper Kuczewski <kucza06@wp.pl>
2019-06-06 12:35:59 +0200

    Updates

---
+++ precache-manifest.f6e127cd0f3d158a156739aa8e3bc164.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4669b4ae9bc8b3bf94f2264fb8dbfdb",
+    "url": "/clicker/index.html"
+  },
+  {
+    "revision": "52891f87e34305106da1",
+    "url": "/clicker/static/js/2.dc42bda9.chunk.js"
+  },
+  {
+    "revision": "b866a5621ec90653d8fb",
+    "url": "/clicker/static/js/main.0779c7ee.chunk.js"
+  },
+  {
+    "revision": "6e4e176355e4075678fe",
+    "url": "/clicker/static/js/runtime~main.b35d8f1a.js"
+  },
+  {
+    "revision": "5fe641e638cfb531df7d9c38e233b08e",
+    "url": "/clicker/static/media/bee.5fe641e6.png"
+  },
+  {
+    "revision": "4f4dd3602331d24b20fe9e1de8b996a4",
+    "url": "/clicker/static/media/bee2.4f4dd360.png"
+  },
+  {
+    "revision": "6fd0f3766224144ae4583becb2e230e9",
+    "url": "/clicker/static/media/bee3.6fd0f376.png"
+  },
+  {
+    "revision": "3f80da53c2783f6c04cedbff12c6b705",
+    "url": "/clicker/static/media/bee4.3f80da53.png"
+  },
+  {
+    "revision": "7783f33c7ba4c18fd7d4a1e26f707ebf",
+    "url": "/clicker/static/media/bee5.7783f33c.png"
+  },
+  {
+    "revision": "cee6b5031bed60e36b431c0a4d794144",
+    "url": "/clicker/static/media/bee6.cee6b503.png"
+  }
+]);
